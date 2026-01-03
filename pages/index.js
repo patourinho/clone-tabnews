6ccd2f8d1554e2934e1a4033d4447083b5ba3d52
Patrick Tourinho <patourinho@gmail.com>
2026-01-03 23:32:38 +0000

    adciona scripts `lint:check` e `lint:write`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>Teste novo </h1>
+  return <h1>Teste novo </h1>;
 }
 
+function Teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
